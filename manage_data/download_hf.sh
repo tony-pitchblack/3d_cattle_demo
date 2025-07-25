c4a79c8fe272779f8d2cb9cc2923ecd77d2e5d5d
Anton Chernov <tony.pitchblack@yandex.ru>
2025-07-26 00:18:44 +0300

    fix data management scripts

--- manage_data/download_hf.sh
+++ manage_data/download_hf.sh
@@ -1,2 +1,7 @@
-source ./.env
-huggingface-cli download exponenta/3d_cattle_demo --local-dir=data --token=$HF_TOKEN --repo-type=dataset
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$SCRIPT_DIR/../.env"
+huggingface-cli download \
+  exponenta/3d_cattle_demo \
+  --local-dir="$SCRIPT_DIR/../data" \
+  --token="$HF_TOKEN" \
+  --repo-type=dataset
--- manage_data/upload_hf.sh
+++ manage_data/upload_hf.sh
@@ -1,2 +1,8 @@
-source ./.env
-huggingface-cli upload exponenta/3d_cattle_demo data/ . --repo-type=dataset --token=$HF_TOKEN
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$SCRIPT_DIR/../.env"
+huggingface-cli upload \
+  exponenta/3d_cattle_demo \
+  "$SCRIPT_DIR/../data" \
+  . \
+  --repo-type=dataset \
+  --token="$HF_TOKEN"
