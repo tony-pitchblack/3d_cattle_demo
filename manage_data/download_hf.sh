94d136cbb1237e55f0be8e9cf8450678ddb86398
Anton Chernov <tony.pitchblack@yandex.ru>
2025-07-26 00:37:43 +0300

    fix download/upload script: treat hf repo as git repo

--- manage_data/download_hf.sh
+++ manage_data/download_hf.sh
@@ -1,7 +1,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$SCRIPT_DIR/../.env"
-huggingface-cli download \
-  exponenta/3d_cattle_demo \
-  --local-dir="$SCRIPT_DIR/../data" \
-  --token="$HF_TOKEN" \
-  --repo-type=dataset
+git clone \
+  git@hf.co:datasets/exponenta/3d_cattle_demo \
+  "$SCRIPT_DIR/../data"
--- manage_data/upload_hf.sh
+++ manage_data/upload_hf.sh
@@ -1,8 +1,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$SCRIPT_DIR/../.env"
-huggingface-cli upload \
-  exponenta/3d_cattle_demo \
-  "$SCRIPT_DIR/../data" \
-  . \
-  --repo-type=dataset \
-  --token="$HF_TOKEN"
+git -C "$SCRIPT_DIR/../data" push \
+  git@hf.co:datasets/exponenta/3d_cattle_demo \
+  HEAD
